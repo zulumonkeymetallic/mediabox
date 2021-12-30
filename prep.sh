71a701ac5b5a3e763153eb9d154facacd2c058d9
zulumonkeymetallic <94960648+zulumonkeymetallic@users.noreply.github.com>
2021-12-30 14:13:37 +0000

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-# See if we need to check GIT for updates
-    # Stash any local changes to the base files
-    git stash > /dev/null 2>&1
-    printf "Updating your local copy of Mediabox.\\n\\n"
-    # Pull the latest files from Git
-    git pull
-    # Check to see if this script "prep.sh" was updated and restart it if necessary
-    changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
-    check_run() {
-        echo "$changed_files" | grep --quiet "$1" && eval "$2"
-    }
-    # Provide a message once the Git check/update  is complete
-    if [ -z "$changed_files" ]; then
-        printf "Your Mediabox is current - No Update needed.\\n\\n"
-    else
-        printf "Mediabox Files Update complete.\\n\\nThis script will restart if necessary\\n\\n"
-    fi
-    # Rename the .env file so this check fails if mediabox.sh needs to re-launch
-    mv .env 1.env
-    read -r -p "Press any key to continue... " -n1 -s
-    printf "\\n\\n"
-    # Run exec mediabox.sh if mediabox.sh changed
-    check_run mediabox.sh "exec ./prep.sh"
+if [ "$EUID" -eq 0 ]
+  then echo "Please do not run this script as root or using sudo"
+  exit
+fi
 
 # # ---Create user account----
 
@@ -33,10 +14,6 @@
       sudo useradd -m plex -u 1000 -g 1000
       sudo echo castleredTuesday6! | passwd plex --stdin
     fi
-
-
-
-
 
 
 
